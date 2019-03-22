91cba086150ea0d7f643c447aa52e429d2cc7cae
kei_taniguchi <kotoura5545@gmail.com>
2019-03-22 14:25:05 +0900

    f

--- app/controllers/adconsiderations_controller.rb
+++ app/controllers/adconsiderations_controller.rb
@@ -1,8 +1,5 @@
 class AdconsiderationsController < ApplicationController
 
-  def index
-  end
-
   def show
     @article = Consideration.find(params[:id])
   end
@@ -18,25 +15,128 @@ class AdconsiderationsController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def update
-    
+
   end
 
   def delete
-    
+
   end
 
   def consideration_params
     params.require(:consideration).permit(:title, :detail)
   end
 
+  def index
+    options = {
+      bucket: 'bucket_name',
+      region: 'us-east-1',
+      keyStart: 'uploads/',
+      acl: 'public-read',
+      accessKey: 'aws_access_key',
+      secretKey: 'aws_secret_key'
+    }
+
+    @aws_data = FroalaEditorSDK::S3.data_hash(options)
+  end
+
+  # Upload file.
+  def upload_file
+    render :json => FroalaEditorSDK::File.upload(params, "public/uploads/files/", { 
+      file_access_path: '/uploads/' 
+    })
+  end
+
+  # Validate if file is smaller than 10Mb.
+  def upload_file_validation
+    render :json => FroalaEditorSDK::File.upload(params, "public/uploads/files/", {
+      validation: Proc.new do |file, type|
+        if File.size(file) > 10 * 1024 * 1024
+          raise 'File size exceeded'
+        end
+      end,
+      file_access_path: '/uploads/'
+    })
+  end
+
+  # Delete file.
+  def delete_file
+    render :json => FroalaEditorSDK::File.delete(params[:src], "public/uploads/files/")
+  end
+
+  # Upload image.
   def upload_image
-    p 'test'
-    p params
-    render FroalaEditorSDK::Image.upload(params, "public/uploads/images/")
+    render :json => FroalaEditorSDK::Image.upload(params, "public/uploads/images/", { 
+      file_access_path: '/uploads/' 
+    })
+  end
+
+  # Upload image.
+  def upload_image_jpeg
+    render :json => FroalaEditorSDK::Image.upload(params, "public/uploads/images/", {
+      validation: {
+        allowedExts: ['.jpeg', '.jpg'],
+        allowedMimeTypes: ['image/jpeg', 'image/pjpeg']
+      },
+      file_access_path: '/uploads/'
+    })
+  end
+
+  # Upload image.
+  def upload_image_resize
+    render :json => FroalaEditorSDK::Image.upload(params, "public/uploads/images/", {
+      resize: {
+        height: 300,
+        width: 600
+      },
+      file_access_path: '/uploads/'
+    })
+  end
+
+  # Validate if image is square.
+  def upload_image_validation
+    render :json => FroalaEditorSDK::Image.upload(params, "public/uploads/images/", {
+      validation: Proc.new do |file, type|
+        img = MiniMagick::Image.open(file)
+        if img.width != img.height
+          raise 'Image is not square'
+        end
+      end,
+      file_access_path: '/uploads/'
+    })
+  end
+
+  # Load images.
+  def load_images
+    render :json => FroalaEditorSDK::Image.load_images("public/uploads/images/", {
+      file_access_path: '/uploads/' 
+    })
+  end
+
+  # Delete image.
+  def delete_image
+    render :json => FroalaEditorSDK::Image.delete(params[:src], "public/uploads/images/")
+  end
+
+  # Upload video.
+  def upload_video
+    render :json => FroalaEditorSDK::Video.upload(params, "public/uploads/videos/", { 
+      file_access_path: '/uploads/' 
+    })
+  end
+
+  # Access uploaded files.
+  def access_file
+    if File.exists?(Rails.root.join('public', 'uploads', 'images', params[:name]))
+      File.open(Rails.root.join('public', 'uploads', 'images', params[:name]), 'rb') do |f|
+        send_data f.read, :filename => ::File.basename(params[:name]), :disposition => "inline"
+      end
+    else
+      render :nothing => true
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
-  root "character#index"
-  post '/upload_image' => 'adconsiderations#upload_image', :as => :upload_image
-  resources :adcharacters
-  resources :characters, only: [:index, :show]
-  resources :considerations, only: [:index, :show]
-  resources :adconsiderations
+	root "character#index"
+	resources :adcharacters
+	resources :characters, only: [:index, :show]
+	resources :considerations, only: [:index, :show]
+	resources :adconsiderations
+	post '/upload_image' => 'adconsiderations#upload_image', :as => :froala_upload_image
+	post '/upload_image_jpeg' => 'adconsiderations#upload_image_jpeg', :as => :froala_upload_image_jpeg
+	post '/upload_image_resize' => 'adconsiderations#upload_image_resize', :as => :froala_upload_image_resize
+	post '/upload_image_validation' => 'adconsiderations#upload_image_validation', :as => :froala_upload_image_validation
+	post '/delete_image' => 'adconsiderations#delete_image', :as => :froala_delete_image
+
+	post '/upload_file' => 'adconsiderations#upload_file', :as => :froala_upload_file
+	post '/upload_file_validation' => 'adconsiderations#upload_file_validation', :as => :froala_upload_file_validation
+	post '/delete_file' => 'adconsiderations#delete_file', :as => :froala_delete_file
+
+	post '/upload_video' => 'adconsiderations#upload_video', :as => :froala_upload_video
+
+	get '/uploads/:name' => 'adconsiderations#access_file', :as => :froala_upload_access_file, :name => /.*/
+	get '/load_images' => 'adconsiderations#load_images', :as => :froala_load_images
 end
+
